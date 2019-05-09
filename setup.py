aca630a82128e83f823afe15b1e3a883fea2b544
Manuel Manso <manel_manso@hotmail.com>
2019-05-09 14:15:17 +0100

    Add README to pypi

--- setup.py
+++ setup.py
@@ -4,12 +4,17 @@
 from setuptools import setup, find_packages
 import re
 
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
+
 setup(
 	name='Python video annotator - module - smooth paths',
-	version="0.2",
+	version="0.3",
 	description="""""",
 	author=['Ricardo Ribeiro'],
 	author_email='ricardojvr@gmail.com',
 	url='https://bitbucket.org/fchampalimaud/pythonvideoannotator-module-smoothpaths',
+	long_description = long_description,
+    long_description_content_type = 'text/markdown',
 	packages=find_packages(),	
 )
